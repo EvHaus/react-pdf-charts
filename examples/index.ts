6593a5274c1013142e3f80e072e411dd1e8e31a0
Ev Haus <ev@haus.gg>
2023-07-05 21:55:13 -0700

    chore: silence react warnings during example builds

--- examples/index.ts
+++ examples/index.ts
@@ -1,2 +1,23 @@
-import './basic';
-import './composed';
+// Silence `useLayoutEffect does nothing on the server` warnings. These come
+// from `recharts` but they're harmless and just clutter the console output.
+const consoleError = console.error;
+console.error = function (message) {
+	if (
+		message.startsWith('Warning: useLayoutEffect does nothing on the server')
+	) {
+		return;
+	}
+	// @ts-expect-error Skipping type checking
+	// rome-ignore lint/style/noArguments: Monkeypatching on purpose
+	consoleError.apply(console, arguments);
+};
+
+const main = async () => {
+	await import('./basic');
+	await import('./composed');
+
+	// rome-ignore lint/nursery/noConsoleLog: <explanation>
+	return console.log('✅ /examples updated!');
+};
+
+main();
